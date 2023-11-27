3ca6d006d6d5071377a9a1a32145980dac5e47a3
elderlyman <ryan.eavey@clapforencore.com>
2023-11-27 14:06:30 -0800

    add debug line

--- netlify/functions/getSeatGeekEvents.js
+++ netlify/functions/getSeatGeekEvents.js
@@ -6,6 +6,18 @@ exports.handler = async (event, context) => {
     const daysFromNow = event.queryStringParameters.daysFromNow;
     const client_id = event.queryStringParameters.client_id;
 
+
+    if (!eventLocation || !daysFromNow || !client_id) {
+
+        throw new Error('Missing required parameters.'
+            //show missig parameters in error message
+            + ' eventLocation: ' + eventLocation
+            + ' daysFromNow: ' + daysFromNow
+            + ' client_id: ' + client_id
+        );
+
+    }
+
     try {
 
         const response = await fetch(
